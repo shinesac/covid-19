f25760b655a28468097d5b1b1c2c475d22b2c058
Shinesa Chowdhury <shinesac@yahoo.com>
2020-06-19 18:02:34 -0400

    added function to remove title

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+
 function myFunction() {
+  var pageTitle = document.getElementById("title");  
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
+    pageTitle.style.display = "none";
   }
 } 
