c01b304c8f9be74b0d6b54beaf04ba316d17d422
Shinesa Chowdhury <shinesac@yahoo.com>
2020-07-20 11:06:11 -0400

    major revisions and add new webpage

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu */
 
-function myFunction() {
-  var pageTitle = document.getElementById("title");  
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-    pageTitle.style.display = "none";
-  }
+function onClickMenu(){
+	document.querySelector(".menu").classList.toggle("change");
+	document.getElementById("navigation").classList.toggle("change");
+}
+
+// create success message for submit button
+
+function submitSuccess(){
+	document.querySelector(".submit").addEventListener("click");
 }
